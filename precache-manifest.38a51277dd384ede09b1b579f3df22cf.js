55740d8e5c750e5ed16d0d4b7cd3399d2410bb79
박은혜 <pehwish@gmail.com>
2020-03-29 02:17:44 +0900

    Updates

---
+++ precache-manifest.38a51277dd384ede09b1b579f3df22cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8630ed09fa0862d771341d78492bb",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "3d06e3492403947c6fa0",
+    "url": "/movie-app/static/css/main.c698557a.chunk.css"
+  },
+  {
+    "revision": "64b69f08bb7c43a0318f",
+    "url": "/movie-app/static/js/2.1f5f1e89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.1f5f1e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d06e3492403947c6fa0",
+    "url": "/movie-app/static/js/main.e5ee3a51.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
